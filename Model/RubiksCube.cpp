2bdb4fad322d49943d868b01075a1a26a4e0d433
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-24 14:30:22 +0530

    Implement getColorLetter

--- Model/RubiksCube.cpp
+++ Model/RubiksCube.cpp
@@ -9,6 +9,23 @@ void RubiksCube::print() const {
 
 }
 
+char RubiksCube::getColorLetter(RubiksCube::COLOR color) {
+    switch (color) {
+        case COLOR::WHITE:
+            return 'w';
+        case COLOR::RED:
+            return 'r';
+        case COLOR::BLUE:
+            return 'b';
+        case COLOR::GREEN:
+            return 'g';
+        case COLOR::YELLOW:
+            return 'y';
+        case COLOR::ORANGE:
+            return 'o';
+    }
+}
+
 vector<RubiksCube::MOVE> RubiksCube::randomShuffle(unsigned int times) {
     return vector<RubiksCube::MOVE>();
 }
