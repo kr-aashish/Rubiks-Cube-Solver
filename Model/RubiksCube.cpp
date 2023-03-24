33c0e79a36847f356cadf0d9a667ad09efce394b
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-24 13:52:03 +0530

    Refactor header functions

--- Model/RubiksCube.cpp
+++ Model/RubiksCube.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "RubiksCube.h"
+#include "vector"
+
+void RubiksCube::print() const {
+
+}
+
+vector<RubiksCube::MOVE> RubiksCube::randomShuffle(unsigned int times) {
+    return vector<RubiksCube::MOVE>();
+}
+
+RubiksCube &RubiksCube::invertMove(RubiksCube::MOVE move) {
+    return this->d();
+}
+
+string RubiksCube::getCornerColourString(uint8_t ind) const {
+    return std::string();
+}
+
+string RubiksCube::getCornerIndex(uint8_t ind) const {
+    return std::string();
+}
+
+string RubiksCube::getCornerOrientation(uint8_t ind) const {
+    return std::string();
+}
