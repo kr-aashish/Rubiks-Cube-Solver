9f5b2894abd91deefa2bf6fa642e6dab61aea7e3
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-24 12:23:40 +0530

    initial setup

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/aashishkumar/Documents/RubicsCubeSolver
+# Install script for directory: /Users/aashishkumar/Documents/RubiksCubeSolver
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/aashishkumar/Documents/RubicsCubeSolver/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/aashishkumar/Documents/RubiksCubeSolver/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
