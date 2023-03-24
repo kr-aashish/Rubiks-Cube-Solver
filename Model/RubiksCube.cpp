f96c9eb2e1c247c2a2cc49bd687e72de2cd38d98
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-24 14:38:18 +0530

    Implement getMove function

--- Model/RubiksCube.cpp
+++ Model/RubiksCube.cpp
@@ -26,6 +26,48 @@ char RubiksCube::getColorLetter(RubiksCube::COLOR color) {
     }
 }
 
+
+string RubiksCube::getMove(RubiksCube::MOVE move) {
+    switch (move) {
+        case MOVE::F:
+            return "F";
+        case MOVE::FPRIME:
+            return "F'";
+        case MOVE::F2:
+            return "F2";
+        case MOVE::B:
+            return "B";
+        case MOVE::BPRIME:
+            return "B'";
+        case MOVE::B2:
+            return "B2";
+        case MOVE::L:
+            return "L";
+        case MOVE::LPRIME:
+            return "L'";
+        case MOVE::L2:
+            return "L2";
+        case MOVE::R:
+            return "R";
+        case MOVE::RPRIME:
+            return "R'";
+        case MOVE::R2:
+            return "R2";
+        case MOVE::U:
+            return "U";
+        case MOVE::UPRIME:
+            return "U'";
+        case MOVE::U2:
+            return "U2";
+        case MOVE::D:
+            return "D";
+        case MOVE::DPRIME:
+            return "D'";
+        case MOVE::D2:
+            return "D2";
+    }
+}
+
 vector<RubiksCube::MOVE> RubiksCube::randomShuffle(unsigned int times) {
     return vector<RubiksCube::MOVE>();
 }
