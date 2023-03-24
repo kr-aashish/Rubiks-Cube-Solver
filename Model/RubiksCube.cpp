eab5b7a3a6556e2db36dda7dff0bffebd7148424
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-24 14:38:44 +0530

    Implement performMove function

--- Model/RubiksCube.cpp
+++ Model/RubiksCube.cpp
@@ -68,6 +68,47 @@ string RubiksCube::getMove(RubiksCube::MOVE move) {
     }
 }
 
+RubiksCube &RubiksCube::performMove(RubiksCube::MOVE move) {
+    switch (move) {
+        case MOVE::F:
+            return this->f();
+        case MOVE::FPRIME:
+            return this->fPrime();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->b();
+        case MOVE::BPRIME:
+            return this->bPrime();
+        case MOVE::B2:
+            return this->b2();
+        case MOVE::L:
+            return this->l();
+        case MOVE::LPRIME:
+            return this->lPrime();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->r();
+        case MOVE::RPRIME:
+            return this->rPrime();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->u();
+        case MOVE::UPRIME:
+            return this->uPrime();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->d();
+        case MOVE::DPRIME:
+            return this->dPrime();
+        case MOVE::D2:
+            return this->d2();
+    }
+}
+
 vector<RubiksCube::MOVE> RubiksCube::randomShuffle(unsigned int times) {
     return vector<RubiksCube::MOVE>();
 }
